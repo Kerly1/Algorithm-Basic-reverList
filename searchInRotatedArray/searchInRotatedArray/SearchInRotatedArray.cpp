80d295a2ed4a5726c16d693770fcca605949cf7a
Kerly <jiangsiyue@outlook.com>
2015-07-31 15:48:45 +0800

    fix

--- searchInRotatedArray/searchInRotatedArray/SearchInRotatedArray.cpp
+++ searchInRotatedArray/searchInRotatedArray/SearchInRotatedArray.cpp
@@ -3,18 +3,20 @@
 #include<string>
 using namespace std;
 
-int main(){
-    cout << "Please input the roated array seperated with enter and end with 'end'." << endl;
-    vector<int> roatedArray;
-    vector<int>::iterator iter = roatedArray.begin();
-    string inputStr;
+int search(vector<int>& nums, int target);
+int binarySearch(vector<int>& nums, vector<int>::iterator left, vector<int>::iterator rignt, int target);
 
-    while (cin >> inputStr){
-        *iter = inputStr;
-    }
+int main(){
     
-    
-    
+
+    cout << "Please input the roated array seperated with enter and end with 'end'." << endl;
+    vector<int> nums1, nums2;
+
+    nums1 = {4, 5, 6, 7, 1, 2, 3};
+    nums1 = { 7, 1, 2, 3, 4, 5, 6 };
+    cout << search(nums1, 1) << search(nums2, 2) << endl;
+
+    return 0;
 }
 
 int search(vector<int>& nums, int target) {
@@ -30,40 +32,40 @@ int search(vector<int>& nums, int target) {
     }
 
     if (minIndex != nums.begin()){
-        if (target > *minIndex && target < nums[len - 1]){
+        if (target >= *minIndex && target < nums[len - 1]){
             //binary search   (minIndex,len-1) target
-            binarySearch(nums, minIndex, nums.begin()+(len - 1), target);
+            return binarySearch(nums, minIndex, nums.begin() + (len - 1), target);
         }
         else{
             //binary search (begin,minIndex-1) target
-            binarySearch(nums, nums.begin(), minIndex - 1, target);
+            return binarySearch(nums, nums.begin(), minIndex - 1, target);
         }
     }
     else{
         //binary search (begin ,end) target
-        binarySearch(nums, nums.begin(), nums.end()-1, target);
+        return binarySearch(nums, nums.begin(), nums.end() - 1, target);
     }
 }
 
 int binarySearch(vector<int>& nums, vector<int>::iterator left, vector<int>::iterator rignt, int target){
-    if (right - left == 0){
+    if ((right - nums.begin()) == (left - nums.begin())){
         if (target != *left){
             return -1;
         }
         else{
-            return left-nums.begin();
+            return left - nums.begin();
         }
     }
     else{
-        vector<int>::iterator mid = left +(right - left)/2;
+        vector<int>::iterator mid = left + (right - left) / 2;
         if (target == *mid){
             return mid - nums.begin();
         }
         else if (target > *mid){
-            binarySearch(nums, mid, right, target);
+            return binarySearch(nums, mid, right, target);
         }
         else(target < *mid){
-            binarySearch(nums, left, mid, target);
+            return binarySearch(nums, left, mid, target);
         }
-    }   
+    }
 }
